34d644ec7a1ad485e08e4ec611e27db7a62e4847
hongsub95 <hong9506@gmail.com>
2021-04-04 00:56:15 +0900

    2021/04/04(00:40AM)

--- users/models.py
+++ users/models.py
@@ -17,8 +17,26 @@ class User(AbstractUser):
         (GENDER_MALE, "Female"),
         (GENDER_MALE, "Other"),
     )
+
+    LANGUAGE_ENGLISH = "en"
+    LANGUAGE_KOREAN = "kr"
+
+    LANGUAGE_CHOICE = ((LANGUAGE_ENGLISH, "English"), (LANGUAGE_KOREAN, "Korean"))
+
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+
+    CURRENCY_CHOICE = ((CURRENCY_USD, "usd"), (CURRENCY_KRW, "krw"))
+    birthday = models.DateField(null=True, blank=True)
     avatar = models.ImageField(null=True, blank=True)
     gender = models.CharField(
         choices=GENDER_CHOICES, max_length=10, null=True, blank=True
     )
+    language = models.CharField(
+        choices=LANGUAGE_CHOICE, max_length=2, null=True, blank=True
+    )
+    currency = models.CharField(
+        choices=CURRENCY_CHOICE, max_length=3, null=True, blank=True
+    )
     bio = models.TextField(default="", blank=True)
+    superhost = models.BooleanField(default=False)
