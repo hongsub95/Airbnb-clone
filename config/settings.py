7b73cb03aa9bc48116144ae8e5af990e4a72fc4f
hongsub95 <hong9506@gmail.com>
2021-10-22 02:38:40 +0900

    2021/10/22

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".elasticbeanstalk.com", "http://127.0.0.1:8000/"]
+ALLOWED_HOSTS = [
+    "airbnb-clone.eba-mbuktp8e.ap-northeast-2.elasticbeanstalk.com",
+    "127.0.0.1:8000",
+]
 
 
 # Application definition
