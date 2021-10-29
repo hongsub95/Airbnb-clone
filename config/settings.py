922717eb8c542f4c54db181a84cda090e7da863e
hongsub95 <hong9506@gmail.com>
2021-10-29 19:29:24 +0900

    2021/10/29

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get("DEBUG"))
 
-ALLOWED_HOSTS = [".elasticbeanstalk.com", "localhost"]
+ALLOWED_HOSTS = [".elasticbeanstalk.com", "127.0.0.1:8000"]
 
 
 # Application definition
